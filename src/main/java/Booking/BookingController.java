b91519f6012ce3e87c0334c7a585baab28ea7d31
Cavoli <cavid19991403>
2019-11-10 21:08:04 +0400

    completed a little bit Vol3

--- src/main/java/Booking/BookingController.java
+++ src/main/java/Booking/BookingController.java
@@ -1,4 +1,43 @@
 package Booking;
 
+import java.util.List;
+
 public class BookingController {
+    private BookingServise bookingServise;
+
+    public BookingController() {
+        bookingServise = new BookingServise();
+    }
+
+    public BookingServise getBookingServise() {
+        return bookingServise;
+    }
+
+    public List<Booking> getAllBookings() {
+        return bookingServise.getAllBookings();
+    }
+
+    public void save(Booking booking) {
+        bookingServise.save(booking);
+    }
+
+    public void showBookings() {
+        bookingServise.getAllBookings().forEach(System.out::println);
+    }
+
+    public void delete(int ID) {
+        bookingServise.delete(ID);
+    }
+
+    public void SaveData(String file) {
+        bookingServise.SaveData(file);
+    }
+
+    public void ReadData(String file) {
+        bookingServise.ReadData(file);
+    }
+
+    public int counts() {
+        return bookingServise.getAllBookings().size();
+    }
 }
